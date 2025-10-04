94fc220b34fa402da31d8afb0bc9c3acb15e2c6e
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-10-04 01:14:58 -0400

    Update mod.rs

--- crates/engine/src/ui/panels/blueprint_editor2/mod.rs
+++ crates/engine/src/ui/panels/blueprint_editor2/mod.rs
@@ -98,15 +98,100 @@ pub struct Connection {
     pub to_pin_id: String,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct BlueprintComment {
     pub id: String,
     pub text: String,
+    #[serde(with = "point_serde")]
     pub position: Point<f32>,
+    #[serde(with = "size_serde")]
     pub size: Size<f32>,
+    #[serde(with = "hsla_serde")]
     pub color: Hsla, // Background color
     pub contained_node_ids: Vec<String>, // Nodes fully contained in this comment
+    #[serde(skip)]
     pub is_selected: bool,
+}
+
+// Serde helpers for GPUI types
+mod point_serde {
+    use super::*;
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+    #[derive(Serialize, Deserialize)]
+    struct PointData {
+        x: f32,
+        y: f32,
+    }
+
+    pub fn serialize<S>(point: &Point<f32>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        PointData { x: point.x, y: point.y }.serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Point<f32>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let data = PointData::deserialize(deserializer)?;
+        Ok(Point::new(data.x, data.y))
+    }
+}
+
+mod size_serde {
+    use super::*;
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+    #[derive(Serialize, Deserialize)]
+    struct SizeData {
+        width: f32,
+        height: f32,
+    }
+
+    pub fn serialize<S>(size: &Size<f32>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        SizeData { width: size.width, height: size.height }.serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Size<f32>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let data = SizeData::deserialize(deserializer)?;
+        Ok(Size::new(data.width, data.height))
+    }
+}
+
+mod hsla_serde {
+    use super::*;
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+    #[derive(Serialize, Deserialize)]
+    struct HslaData {
+        h: f32,
+        s: f32,
+        l: f32,
+        a: f32,
+    }
+
+    pub fn serialize<S>(color: &Hsla, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        HslaData { h: color.h, s: color.s, l: color.l, a: color.a }.serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Hsla, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let data = HslaData::deserialize(deserializer)?;
+        Ok(Hsla { h: data.h, s: data.s, l: data.l, a: data.a })
+    }
 }
 
 impl BlueprintComment {
