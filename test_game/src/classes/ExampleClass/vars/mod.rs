2838632f1d431ae8b8698289f54df74d5323032b
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-10-14 17:46:58 -0400

    Update mod.rs

--- test_game/src/classes/ExampleClass/vars/mod.rs
+++ test_game/src/classes/ExampleClass/vars/mod.rs
@@ -5,6 +5,6 @@ use std::cell::Cell;
 use std::cell::RefCell;
 
 thread_local! {
-    pub static DSFASDF: RefCell<(f32, f32, f32)> = RefCell::new(Default::default());
+    pub static DSFASDF: RefCell<String> = RefCell::new(String::new());
 }
 
