facf8b9530d0d9f8d447447b667b78c4a5c04955
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-10-25 23:59:43 -0400

    Update menu.rs

--- crates/engine/src/ui/menu.rs
+++ crates/engine/src/ui/menu.rs
@@ -313,7 +313,7 @@ impl Render for AppTitleBar {
                             .small()
                             .ghost()
                             .on_click(|_, _, cx| {
-                                cx.open_url("https://github.com/longbridge/gpui-component")
+                                cx.open_url("https://github.com/Far-Beyond-Pulsar/Pulsar-Native")
                             }),
                     )
                     .child(
